015c86c89aa22c0d30ebe2b8d8a193e0481efe51
Marat Reymers <marat.maratori@gmail.com>
2019-08-04 21:58:57 +0300

    Add tests for func literals

--- gochecknonamedreturn/testdata/named_return_function_declaration.go
+++ gochecknonamedreturn/testdata/named_return_function_declaration.go
@@ -1,17 +1,17 @@
 package testdata
 
-func SingleNamedReturn() (x string) { // want `don't use named return values`
+func DeclarationSingleNamedReturn() (x string) { // want `don't use named return values`
 	return ""
 }
 
-func SingleNamedReturnUnderscore() (_ error) { // want `don't use named return values`
+func DeclarationSingleNamedReturnUnderscore() (_ error) { // want `don't use named return values`
 	return nil
 }
 
-func SingleTypeTwoNamedReturns() (err, ignore error) { // want `don't use named return values`
+func DeclarationSingleTypeTwoNamedReturns() (err, ignore error) { // want `don't use named return values`
 	return nil, nil
 }
 
-func SingleTypeTwoNamedReturnsUnderscore() (_, y *int) { // want `don't use named return values`
+func DeclarationSingleTypeTwoNamedReturnsUnderscore() (_, y *int) { // want `don't use named return values`
 	return nil, nil
 }
